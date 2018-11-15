574727b346ecbaa4a4191f9ffede6fa283886440
ocerri <ocerri@caltech.edu>
2018-11-15 11:02:09 -0600

    update to run.sh

--- runDQM.sh
+++ runDQM.sh
@@ -1,3 +1,3 @@
 source ~/.bash_profile
 cd /Users/fqnet1/FNAL_TB/18_11/DataQualityMonitor
-python DQM_SiPM.py -C config/FNAL_TB_1811/VME_v3.txt -S ~/cernbox/ocerri/www/FNAL_TB_1811/ -i ../data/VME/RECO/v3/DataVMETiming_RunXXX.root -N $1
+python DQM_SiPM.py -C config/FNAL_TB_1811/VME_$2.txt -S ~/cernbox/ocerri/www/FNAL_TB_1811/ -i ../data/VME/RECO/$2/DataVMETiming_RunXXX.root -N $1
